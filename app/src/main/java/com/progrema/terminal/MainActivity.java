23372982070d199a2a7d3e5cb08b55816c32dbd6
Progrema Studio <iqbalpakeh@gmail.com>
2021-08-22 18:22:55 +0700

    Add more codes

--- app/src/main/java/com/progrema/terminal/MainActivity.java
+++ app/src/main/java/com/progrema/terminal/MainActivity.java
@@ -2,13 +2,68 @@ package com.progrema.terminal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoyaltyCardReader.AccountCallback {
+
+    public static final String TAG = "CardReaderFragment";
+
+    // Recommend NfcAdapter flags for reading from other Android devices. Indicates that this
+    // activity is interested in NFC-A devices (including other Android devices), and that the
+    // system should not check for the presence of NDEF-formatted data (e.g. Android Beam).
+    public static int READER_FLAGS = NfcAdapter.FLAG_READER_NFC_A | NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK;
+    public LoyaltyCardReader mLoyaltyCardReader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLoyaltyCardReader = new LoyaltyCardReader(this);
+
+        // Disable Android Beam and register our card reader callback
+        enableReaderMode();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        disableReaderMode();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        enableReaderMode();
+    }
+
+    private void enableReaderMode() {
+        Log.i(TAG, "Enabling reader mode");
+        NfcAdapter nfc = NfcAdapter.getDefaultAdapter(this);
+        if (nfc != null) {
+            nfc.enableReaderMode(this, mLoyaltyCardReader, READER_FLAGS, null);
+        }
+    }
+
+    private void disableReaderMode() {
+        Log.i(TAG, "Disabling reader mode");
+        NfcAdapter nfc = NfcAdapter.getDefaultAdapter(this);
+        if (nfc != null) {
+            nfc.disableReaderMode(this);
+        }
+    }
+
+    @Override
+    public void onAccountReceived(final String account) {
+        // This callback is run on a background thread, but updates to UI elements must be performed
+        // on the UI thread.
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                // mAccountField.setText(account);
+            }
+        });
     }
 }
